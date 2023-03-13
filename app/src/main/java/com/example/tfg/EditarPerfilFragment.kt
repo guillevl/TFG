4e593122563df93257aeefed1c9c2f592c0d2d87
guille <guillermovallejolopez@gmail.com>
2023-03-13 20:13:06 +0100

    perfil

---
+++ app/src/main/java/com/example/tfg/EditarPerfilFragment.kt
@@ -0,0 +1,31 @@
+package com.example.tfg
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class EditarPerfilFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_editar_perfil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)?.isVisible = false
+        activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationViewAdmin)?.isVisible = false
+    }
+    override fun onStop() {
+        super.onStop()
+        activity?.findViewById<BottomNavigationView>(R.id.bottomNavigationView)?.isVisible = true
+    }
+}
--- app/src/main/java/com/example/tfg/PerfilFragment.kt
+++ app/src/main/java/com/example/tfg/PerfilFragment.kt
@@ -1,10 +1,14 @@
 package com.example.tfg
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class PerfilFragment : Fragment() {
     override fun onCreateView(
@@ -13,5 +17,32 @@ class PerfilFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Eliminar cuenta")
+            .setMessage("¿Seguro que desea eliminar la cuenta?")
+            .setPositiveButton("Sí") { dialog, which ->
+                // Acciones a realizar si el usuario presiona el botón "Sí"
+                activity?.supportFragmentManager?.beginTransaction()
+                    ?.replace(R.id.container, PantallaInicioFragment())?.commit()
+            }
+            .setNegativeButton("No") { dialog, which ->
+                // Acciones a realizar si el usuario presiona el botón "No"
+            }
+        val alerta = builder.create()
+        view?.findViewById<Button>(R.id.btnEditarPerfil)?.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, EditarPerfilFragment())?.addToBackStack(null)?.commit()
+        }
+        view?.findViewById<Button>(R.id.btnEliminarCuenta)?.setOnClickListener {
+            alerta.show()
+        }
+        view?.findViewById<Button>(R.id.btnCerrarSesion)?.setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.container, LoginFragment())?.commit()
+        }
     }
 }
